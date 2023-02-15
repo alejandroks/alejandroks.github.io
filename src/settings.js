abf7dc7132850e28a2911df0d93e9e03d09d97f5
Alexey <alejandroks02@gmail.com>
2023-02-15 17:22:43 +0300

    - add letter

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[]};
